9b95c815897e4de3ecb78e1598e70079a8649848
Iulian Negrescu <iuliannegrescu@Iulians-MacBook-Pro.local>
2025-07-10 14:49:37 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_instance" "my-example" {
+    ami             = "ami-0becc523130ac9d5d"
+    instance_type   = "t3.micro"
+    tags = {
+        Name = "terraform-my-example"
+    }
+}
