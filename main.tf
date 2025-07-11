ea7398878d7d60eb9221dae219910f58ddf24342
Iulian Negrescu <iulian.negrescu@gmail.com>
2025-07-11 18:04:19 +0200

    Added the variable

--- main.tf
+++ main.tf
@@ -2,14 +2,19 @@ provider "aws" {
     region = "eu-north-1"
 }
 
+variable "server_port" {
+    description     = "The port the server will use for HTTP requests"
+    type            = number
+    default         = 8080
+}
 resource "aws_instance" "my-example" {
-    ami             = "ami-0becc523130ac9d5d"
+    ami             = "ami-042b4708b1d05f512"
     instance_type   = "t3.micro"
     vpc_security_group_ids = [aws_security_group.terraform-my-example-sg.id]
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox http -f -p 8080 &
+                nohup busybox httpd -f -p 8080 &
                 EOF
 
     user_data_replace_on_change = true
@@ -20,8 +25,8 @@ resource "aws_instance" "my-example" {
 resource "aws_security_group" "terraform-my-example-sg" {
     name = "terraform-my-example-sg"
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.server_port
+        to_port     = var.server_port
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
