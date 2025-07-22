6f021001b1c04ecf2810d1ebdb09dfdb269ec99c
Iulian Negrescu <iulian.negrescu@gmail.com>
2025-07-22 18:25:21 +0200

    Added ALB in conjunction with ASG
    This change includes aws_lb, aws_lb_listener and aws_lb_listener_rule, as well a new aws_security_group to allow inbound and outbound traffic throught the load balancer. It includes also a aws_lb_target_group for a health check.

--- main.tf
+++ main.tf
@@ -51,7 +51,11 @@ resource "aws_autoscaling_group" "my-example-asg" {
         id              = aws_launch_template.my-example-lt.id
         version         = "$Latest"
     }
+
     vpc_zone_identifier = data.aws_subnets.default.ids
+    target_group_arns   = [aws_lb_target_group.asg.arn]
+    health_check_type   = "ELB"
+
     min_size            = 2
     max_size            = 10
     tag {
@@ -61,6 +65,80 @@ resource "aws_autoscaling_group" "my-example-asg" {
 }
 }
 
+resource "aws_lb" "my-lb-example" {
+    name                = "terraform-asg-example"
+    load_balancer_type  = "application"
+    subnets             = data.aws_subnets.default.ids
+    security_groups     = [aws_security_group.alb.id]
+}           
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn   = aws_lb.my-lb-example.arn
+    port                = 80
+    protocol            = "HTTP"
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type    = "text/plain"
+        message_body    = "404: page not found"
+        status_code     = 404
+      }
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn    = aws_lb_listener.http.arn
+    priority        = 100
+
+    condition {
+      path_pattern {
+        values = ["*"]
+      }
+    }
+
+    action {
+      type              = "forward"
+      target_group_arn  = aws_lb_target_group.asg.arn
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+    ingress {
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"] 
+    }
+
+    # Allow all outbound requests
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"] 
+    }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name        = "terraform-asg-example"
+    port        = var.server_port
+    protocol    = "HTTP"
+    vpc_id      = data.aws_vpc.default.id
+
+    health_check {
+      path                  = "/"
+      protocol              = "HTTP"
+      matcher               = "200"
+      interval              = 15
+      timeout               = 3
+      healthy_threshold     = 2
+      unhealthy_threshold   = 2
+    }
+}
 
 # resource "aws_instance" "my-example" {
 #     ami             = "ami-042b4708b1d05f512"
@@ -87,7 +165,7 @@ resource "aws_security_group" "instance" {
     }
 }
 
-# output "public_ip" {
-#     value = aws_instance.my-example.public_ip
-#     description = "The public IP address of the web server"
-# }
+output "alb_dns_name" {
+    value       = aws_lb.my-lb-example.dns_name
+    description = "The domain name of the load balancer"
+}
