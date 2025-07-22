cb3e9e7bf7a873e38b4077fc68292756e5437079
Iulian Negrescu <iulian.negrescu@gmail.com>
2025-07-22 18:25:21 +0200

    Added ALB in conjunction with ASG
    This change includes aws_lb, aws_lb_listener and aws_lb_listener_rule, as well a new aws_security_group to allow inbound and outbound traffic throught the load balancer. It includes also a aws_lb_target_group for a health check.

--- main.tf
+++ main.tf
@@ -140,21 +140,6 @@ resource "aws_lb_target_group" "asg" {
     }
 }
 
-# resource "aws_instance" "my-example" {
-#     ami             = "ami-042b4708b1d05f512"
-#     instance_type   = "t3.micro"
-#     vpc_security_group_ids = [aws_security_group.instance.id]
-#     user_data = <<-EOF
-#                 #!/bin/bash
-#                 echo "Hello, World" > index.html
-#                 nohup busybox httpd -f -p ${var.server_port} &
-#                 EOF
-
-#     user_data_replace_on_change = true
-#     tags = {
-#         Name = "terraform-my-example"
-#     }
-# }
 resource "aws_security_group" "instance" {
     name = "terraform-my-example"
     ingress {
